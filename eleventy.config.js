076bca5f82089c287ae43cbae72c09768bd0d92c
River Fox <river.fox@mailbox.org>
2025-03-05 19:59:21 -0600

    add text

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,8 @@
+import ejsPlugin from "@11ty/eleventy-plugin-ejs";
+
 export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(ejsPlugin);
+
   eleventyConfig.addPassthroughCopy("images/**/*")
   eleventyConfig.addPassthroughCopy("*.css")
   eleventyConfig.addPassthroughCopy("CNAME")
